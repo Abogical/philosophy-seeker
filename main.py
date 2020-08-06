e49c2dd4661788e45e75a3fef5ce9b8efbdb9307
Abdelrahman Abdelrahman <abogical@gmail.com>
2020-08-06 15:48:46 +0200

    Consider redirects and entering the philosophy URL itself

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
 		print_usage()
 		exit(1)
 
-	wiki_match = fullmatch('https?://(\w+)\.wikipedia\.org/wiki/(.+)', sys.argv[1])
+	wiki_re = compile('https?://(\w+)\.wikipedia\.org/wiki/(.+)')
+	wiki_match = wiki_re.fullmatch(sys.argv[1])
 	if wiki_match == None:
 		print('Not a valid wikipedia link', file=sys.stderr)
 		print_usage()
@@ -25,13 +26,36 @@ if __name__ == '__main__':
 	wiki_lang = wiki_match.group(1)
 	name = wiki_match.group(2)
 
+	if name == 'Philosophy':
+		print('Already entered the philosophy URL')
+		exit(0)
+
 	name_re = compile('/wiki/([^#]+)')
 
-	visited = [name]
+	visited = []
+
+	def detect_loop(name):
+		if name in visited:
+			print(f'Loop detected: {" -> ".join(visited[visited.index(name):])} -> {name}')
+			exit(0)
+
+	def detect_philosophy(name):
+		if name == 'Philosophy':
+			print(f'Found philosophy wiki! Link Path: \n{" -> ".join(visited)} -> Philosophy'
+				if len(visited) > 0 else 'Already entered a URL that redirected to philosophy')
+			exit(0)
+
+
 	for _ in range(1, URL_LIMIT):
 		next_name = None
+		response = urlopen(f'https://{wiki_lang}.wikipedia.org/wiki/{name}')
+		# Redirect may change the name of the loop
+		name = wiki_re.fullmatch(response.geturl()).group(2)
+		detect_philosophy(name)
+		detect_loop(name)
+		visited.append(name)
 		# Determining if it's a "normal" link
-		for link in fromstring(urlopen(f'https://{wiki_lang}.wikipedia.org/wiki/{name}').read()).xpath('//*[@role="main"]//p/a[starts-with(@href, "/wiki/")]'):
+		for link in fromstring(response.read()).xpath('//*[@role="main"]//p/a[starts-with(@href, "/wiki/")]'):
 			parens = 0
 			# Check if it is inside a paranthesis
 			for token in reversed(link.xpath('./preceding-sibling::text()')):
@@ -45,16 +69,11 @@ if __name__ == '__main__':
 				continue
 			next_name = name_re.match(link.attrib['href']).group(1)
 			break
-		if next_name == 'Philosophy':
-			print(f'Found philosophy wiki! Link Path: \n{" -> ".join(visited)} -> Philosophy')
-			exit(0)
-		if next_name in visited:
-			print(f'Loop detected: {" -> ".join(visited[visited.index(next_name):])} -> {next_name}')
-			exit(0)
-		visited.append(next_name)
+		detect_philosophy(next_name)
+		detect_loop(next_name)
 		name = next_name
-		print(f'Visiting topic {name}')
 		sleep(0.5)
+		print(f'Visiting topic {name}')
 
 
 
